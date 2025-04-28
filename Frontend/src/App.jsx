79ade3e1a8a17dee77fcb4c3a4f4a3023a976025
ISHA0i <ishachovatiya2203@gmail.com>
2025-04-28 20:42:32 +0530

    css updation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,17 +4,7 @@ import CybersecurityAuditForm from './component/CybersecurityAuditForm';
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: 'var(--primary-dark)',
-        color: 'var(--neutral-light-3)',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <div className="App">
       <CybersecurityAuditForm />
     </div>
   );

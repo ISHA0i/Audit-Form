7e068b0ec295bba082dec759edb0fbfd5732b645
ISHA0i <ishachovatiya2203@gmail.com>
2025-04-28 17:46:52 +0530

    final set up

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,23 @@
 // import { useState } from 'react'
-import './App.css'
-import CybersecurityAuditForm from './CybersecurityAuditForm'
+import './App.css';
+import CybersecurityAuditForm from './component/CybersecurityAuditForm';
 
 function App() {
- 
-
   return (
-    <> 
-    <CybersecurityAuditForm />
-    </>
-  )
+    <div
+      className="App"
+      style={{
+        backgroundColor: 'var(--primary-dark)',
+        color: 'var(--neutral-light-3)',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <CybersecurityAuditForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
